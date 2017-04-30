d48b061bb804462f171ea38c2fe380fb17388ac0
venomfly1041 <etrian_blave@yahoo.co.jp>
2017-04-30 14:58:51 +0900

    added .htaccess

--- blog/php/favicon.php
+++ blog/php/favicon.php
@@ -1,6 +1,8 @@
 
 <?php
-require_once(config.php);
+require_once("config.php");
 $cfg=new config();
 
-<link rel="shortcut icon" href="<?php echo cfg->img_file . cfg->favicon_img    ?>">
+?>
+
+<link rel="shortcut icon" href=<?php echo $cfg->img_file . $cfg->favicon_img;?>>
